fcb9d8718f9e5858b7a97a24e6564b30a51ebab0
sillyboyy <3331553521@qq.com>
2024-11-21 21:33:05 +0800

    更改为黑马的home,goodsitem

--- src/views/layout/home.vue
+++ src/views/layout/home.vue
@@ -1,41 +1,55 @@
 <template>
   <div class="home">
-    <van-nav-bar
-      title="智慧商城"
-    />
+    <!-- 导航条 -->
+    <van-nav-bar fixed title="智慧商城"/>
+
+    <!-- 搜索框 -->
     <van-search
-      v-model="searchValue"
       background="#f1f1f2"
-      placeholder="请输入搜索关键词"
+      placeholder="请在此输入搜索关键词"
       readonly
       shape="round"
       @click="$router.push('/search')"
     />
+
+    <!-- 轮播图 -->
     <van-swipe :autoplay="3000" class="my-swipe" indicator-color="white">
-      <van-swipe-item><img src="@/assets/banner1.jpg"/></van-swipe-item>
-      <van-swipe-item><img src="@/assets/banner2.jpg"/></van-swipe-item>
-      <van-swipe-item><img src="@/assets/banner3.jpg"/></van-swipe-item>
+      <van-swipe-item>
+        <img alt="" src="@/assets/banner1.jpg">
+      </van-swipe-item>
+      <van-swipe-item>
+        <img alt="" src="@/assets/banner2.jpg">
+      </van-swipe-item>
+      <van-swipe-item>
+        <img alt="" src="@/assets/banner3.jpg">
+      </van-swipe-item>
     </van-swipe>
-    <van-grid :column-num="5">
+
+    <!-- 导航 -->
+    <van-grid column-num="5" icon-size="40">
       <van-grid-item
-        v-for="value in 10"
-        :key="value"
+        v-for="item in 10" :key="item"
         icon="https://smart-shop.itheima.net/uploads/10001/20230320/58a7c1f62df4cb1eb47fe83ff0e566e6.png"
         text="新品首发"
+        @click="$router.push('/category')"
       />
     </van-grid>
 
+    <!-- 主会场 -->
     <div class="main">
       <img alt="" src="@/assets/main.png">
     </div>
 
+    <!-- 猜你喜欢 -->
     <div class="guess">
-      <p class="guess-title">-- 猜你喜欢 --</p>
+      <p class="guess-title">—— 猜你喜欢 ——</p>
+
       <div class="goods-list">
         <GoodsItem v-for="item in 10" :key="item"></GoodsItem>
       </div>
     </div>
   </div>
+
 </template>
 
 <script>
@@ -46,32 +60,65 @@ export default {
   name: 'HomeIndex',
   components: { GoodsItem },
   data () {
-    return {
-      searchValue: ''
-    }
+    return {}
   }
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+// 主题 padding
+.home {
+  padding-top: 100px;
+  padding-bottom: 50px;
+}
+
+// 导航条样式定制
 .van-nav-bar {
-  background-color: rebeccapurple;
+  z-index: 999;
+  background-color: #c21401;
+
+  ::v-deep .van-nav-bar__title {
+    color: #fff;
+  }
 }
 
-::v-deep .van-nav-bar__title {
+// 搜索框样式定制
+.van-search {
+  position: fixed;
+  width: 100%;
+  top: 46px;
+  z-index: 999;
+}
+
+// 分类导航部分
+.my-swipe .van-swipe-item {
+  height: 185px;
   color: #fff;
-}
-
-img {
-  width: 375px;
-}
-
-::v-deep .van-icon {
-  font-size: 40px;
-}
-
-.guess-title {
+  font-size: 20px;
   text-align: center;
-  color: #777;
+  background-color: #39a9ed;
+}
+
+.my-swipe .van-swipe-item img {
+  width: 100%;
+  height: 185px;
+}
+
+// 主会场
+.main img {
+  display: block;
+  width: 100%;
+}
+
+// 猜你喜欢
+.guess .guess-title {
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+}
+
+// 商品样式
+.goods-list {
+  background-color: #f6f6f6;
 }
 </style>
